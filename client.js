d749e995a17c76d553084211aa9fb5443edc30c7
Angela Zhou <a03zhou@gmail.com>
2023-07-16 01:36:10 -0400

    typo

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const net = require("net");
 const { IP, PORT } = require("./constants");
 
 const connect = function() {
-  const conn = net.createConnection({host: IP, port: PORT });
+  const conn = net.createConnection({ host: IP, port: PORT });
 
   conn.on('data', (message) => {
     console.log('The server message: ', message);
