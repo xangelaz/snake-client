8c8b7fa18962a0d7ec0bba11194e8ed01f7716a4
Angela Zhou <99564861+xangelaz@users.noreply.github.com>
2023-07-16 00:32:45 -0400

    Update play.js
    removed extra comments

--- play.js
+++ play.js
@@ -8,16 +8,3 @@ setupInput(conn);
 
 
 console.log("Connecting ...");
-
-
-
-// // code below is from cloning
-// process.stdout.write("\x07");
-
-// const { Game } = require("./src/Game")
-// const { UserInterface } = require("./src/UserInterface")
-// const { RemoteInterface } = require("./src/RemoteInterface")
-// const game = new Game(new UserInterface(), new RemoteInterface())
-
-// // Begin game
-// game.start()
